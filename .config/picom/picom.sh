1ad85dee798b24255e624afa76d108b747decd12
powerfuldeveloper <husseinmirzaki@gmail.com>
2024-12-28 20:30:17 +0330

    [stage] better config

--- .config/picom/picom.sh
+++ .config/picom/picom.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pkill -f /bin/picom/build/src/picom
-$HOME/bin/picom/build/src/picom --config $HOME/.config/picom/picom.conf -b
+$HOME/path/picom/build/src/picom --config $HOME/.config/picom/picom.conf -b
 echo "running picom !"
 
